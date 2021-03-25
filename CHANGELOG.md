0005c596026e583da9317bb35eb9cbdaad04a1b6
Nguyen Luu <ncluu95@gmail.com>
2021-03-25 22:24:09 +0700

    add document and web support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.2
+
+* Add document and support web
+
 ## 0.0.1
 
 * TODO: Initial release.
---
+++ lib/overscroll_pop_web.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+class OverscrollPopWeb {
+  static void registerWith(Registrar registrar) {}
+}
